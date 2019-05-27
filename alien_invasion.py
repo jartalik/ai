f08bb36dd9aa42669869c1bb47f16cc0f652a255
jartalik <jar.talik@gmail.com>
2019-05-27 20:38:04 +0200

    g

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Group
+import game_functions as gf
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    # Initialize game and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Inwazja Obcych")
+    # Make a ship.
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
+    
+    # Start the main loop for the game.
+    while True:
+        # Watch for keyboard and mouse events.
+        # Redraw the screen during each pass through the loop.
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+run_game()
